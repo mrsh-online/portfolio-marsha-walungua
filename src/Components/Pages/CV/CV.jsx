e881c76b5bd8d76a3efdaca8ad2a77b208963fe6
shango-wk <shango.walungua@gmail.com>
2025-10-23 16:49:23 +0200

    correction bug pdf cv

--- src/Components/Pages/CV/CV.jsx
+++ src/Components/Pages/CV/CV.jsx
@@ -4,9 +4,9 @@ export default function CV({lang}){
   return(<section className="cv" id="cv">
     
             {lang ==="fr" ?
-              <a href="/CV.pdf" target="_blank">Télécharger mon CV</a>
+              <a href="/portfolio-marsha-walungua/CV.pdf" target="_blank">Télécharger mon CV</a>
             :
-            <a href="/CV.pdf" target="_blank">Download Resume</a>
+            <a href="/portfolio-marsha-walungua/CV.pdf" target="_blank">Download Resume</a>
             }
 
     <FilePreview 
