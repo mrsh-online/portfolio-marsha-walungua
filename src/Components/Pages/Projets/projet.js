59d16131c22bd4e6319fa92270b9c6c60e4f9f02
shango-wk <shango.walungua@gmail.com>
2025-12-02 08:23:01 +0100

    ajout lien Notion

--- src/Components/Pages/Projets/projet.js
+++ src/Components/Pages/Projets/projet.js
@@ -3,12 +3,12 @@ const projets = [
   sumaryFr: "Mon homelab est composé d’un répéteur Wi-Fi, d’un routeur dédié, et d’un ancien Mac que j’utilise pour apprendre et expérimenter Arch Linux. J’y ai également un ThinkPad équipé de Proxmox pour la virtualisation et différents tests d’infrastructure. Mon MacBook Air me sert de machine principale, tandis qu’une tablette Samsung Galaxy Tab me permet de rester connecté et de gérer mon environnement lorsque je ne suis pas à la maison.",
   sumaryEn: "My homelab includes a Wi-Fi repeater, a dedicated router, and an old Mac that I use for learning and experimenting with Arch Linux. I also run a ThinkPad with Proxmox for virtualization and various infrastructure tests. My MacBook Air is my primary workstation, and I use a Samsung Galaxy Tab to stay connected and manage my setup when I’m away from home.",
   img: "/homelab.webp",
-  link:'https://humanforimpact.com'
+  link:'https://www.notion.so/Homelab-2bdd613a6d538005b358e86a42f737ec?source=copy_link'
   },
   {titre:"Midori",
   sumaryFr: "Midori est mon assistante IA personnelle, conçue pour m’aider tout au long de mes études. Elle fonctionne grâce à un workflow N8N sous Docker, relié à un serveur MCP, et utilise les modèles de Mistral AI. Midori m’aide avec un minuteur Pomodoro, des notes autocollantes, des rappels et des notifications, notamment pour mon serveur Wazuh. Elle est également connectée à mon coffre Obsidian, ce qui lui permet d’interagir avec mes notes, d’organiser mes idées et de centraliser mes informations. Grâce à elle, je reste plus efficace, structuré et productif au quotidien.",
   sumaryEn: "Midori is my personal AI assistant, designed to support me throughout my studies. She runs on an N8N workflow inside Docker, connected to an MCP server, and powered by Mistral AI. Midori helps me with a Pomodoro timer, sticky notes, reminders, and notifications, including alerts from my Wazuh server. She is also connected to my Obsidian vault, allowing her to interact with my notes, organize information, and centralize my knowledge. Thanks to her, I stay more efficient, organized, and productive every day.",
-  link: "https://www.ludovic-breant.fr",
+  link: "https://www.notion.so/Midori-2bdd613a6d538026a1fff7ede582b027?source=copy_link",
   img: "/midori.png"
   }  
 ]
