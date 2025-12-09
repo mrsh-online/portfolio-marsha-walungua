afe747069491c29850e33f9db6174384b5c0fa60
shango-wk <shango.walungua@gmail.com>
2025-12-09 02:48:17 +0100

    changement mail

--- src/Components/Pages/Contacts/Contacts.jsx
+++ src/Components/Pages/Contacts/Contacts.jsx
@@ -3,7 +3,7 @@ function Contacts(){
     <section className="Contacts" id="contact">
       <h1>Contacts</h1>
       <div className="ContactsLinks">
-        <a href="mailto:mrsh-online@proton.me" target="_blank">Mail</a>
+        <a href="mailto:marshawalungua@gmail.com" target="_blank">Mail</a>
         <a href="https://www.linkedin.com/in/marsha-walungua-b53295383" target="_blank">Linkedin</a>
       </div>
     
